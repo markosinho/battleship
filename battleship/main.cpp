cb7cbeecf9172bd67d57a4fcb37057e0e3ab160a
Milos Radosavljevic <kemis93@outlook.com>
2015-04-20 19:23:29 +0200

    Prototip1.01
    Prototip1.01
    Postavlja jedan brod i dozvoljava gadjanje broda

---
+++ battleship/main.cpp
@@ -0,0 +1,77 @@
+// Potapanje brodova.cpp : main project file.
+
+
+#include "Polje.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+void swap(int& a, int& b);
+
+int main()
+{
+
+   vector <Field> row(8, Field());
+   vector <vector <Field> > sea(8, row);
+   // Prvobitan izgled
+   Field::printSea(sea);
+
+//   int x1, y1, x2, y2;
+
+
+
+   /*
+   // Unos koordinata pocetka i kraja
+   cout << "Unesite koordinate pocetka:" << endl;
+   cin >> x1 >> y1;
+   cout << "Unesite koordinate kraja:" << endl;
+   cin >> x2 >> y2;
+
+   // Kreiranje broda
+   Field::createShip(x1, y1, x2, y2, sea);
+
+   */
+   Field::createShipRandom(4, sea);
+
+   int x = 0;
+   int y = 0;
+
+   // Gadjanje
+   for(int i = 1; i < 64; i++)
+   {
+       cout << endl;
+       cout << "Pokusaj " << i << ':' << endl;
+       cout << "Ubaci koordinate x i y:" << endl;
+       cin >> x >> y;
+
+
+       if(x > 7 || y > 7)
+       {
+           cout << "Pogresne koordinate!" << endl;
+           system("PAUSE");
+           return 0;
+       }
+
+       sea[x][y].setHit(true);
+
+       Field::printSea(sea);
+       cout << endl;
+
+   }
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+
+}
+
+void swap(int& a, int& b)
+{
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
---
+++ battleship/polje.cpp
@@ -0,0 +1,175 @@
+
+#include "Polje.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+#include <ctime>	// Seme za rand()
+#include <vector>
+#include <Windows.h>
+
+using namespace std;
+
+// Ako se ne stavi u konstruktoru, hit nece biti false podrazumevano
+Field::Field()
+{
+    hit = false;
+    id = 0;
+}
+
+
+// getters & setters
+bool Field::isHit()
+{
+    return hit;
+}
+
+void Field::setHit(bool x)
+{
+    hit = x;
+}
+
+
+int Field::getId()
+{
+    return id;
+}
+
+void Field::setId(int x)
+{
+    id = x;
+}
+
+// Kako da izbegnemo unosenje dimenzija matrice?
+void Field::createShip(int x1, int y1, int x2, int y2, vector <vector <Field> > &f)
+{
+    /* Kreiranje broda s datim koordinatama pocetka i kraja */
+
+    // ako x1 > x2 ====> for petlja ima drugaciji uslov (i = x2; i <= x1; i++)
+    if((x1 > x2) || (y1 > y2))
+    {
+        swap(x1, x2);
+        swap(y1, y2);
+    }
+
+    // Bar jedan par clanova koordinata mora biti isti
+    if((x1 != x2) & (y1 != y2))
+    {
+        cout << "Pogresan unos koordinata!" << endl;
+        system("PAUSE");
+        // Ovde moze neki exception ili nekako drugacije da se resi
+    }
+
+    int i = 0;
+    // Kreiranje broda
+    if(y1 == y2)
+        for(i = x1; i <= x2; i++)
+            f[i][y1].setId(1);
+    else
+        for(i = y1; i <= y2; i++)
+            f[x1][i].setId(1);
+
+    cout << "Brod je kreiran!" << endl;
+}
+
+// Kreiranje broda zadate velicine na nepoznatim koordinatama
+void Field::createShipRandom(int size, vector <vector <Field> > &f)
+{
+    srand(time(0));
+
+    int x1, x2, y1, y2;
+    bool vertical = false;
+
+    if((rand() % 12) > 5)
+        vertical = true;
+
+
+    x1 = ((rand()*150 + 16) * 21 + 3) % 8;
+    y1 = ((rand()*150 + 16) * 21 + 3) % 8;
+
+    // Provera da li su koordinate pocetka negde gde mora da bude brod horizontalan/vertikalan
+    // Ovo cu veceras da dopunim ;)
+
+    // Random da li ce biti horizontalan ili vertikalan brod
+    if(vertical)
+    {
+        if((x1 + size) > 7)
+        {
+            x2 = x1 - size + 1;
+        }
+        else
+            x2 = x1 + size - 1;
+
+        y2 = y1;
+    }
+    else
+    {
+        if((y1 + size) > 7)
+        {
+            y2 = y1 - size + 1;
+        }
+        else
+            y2 = y1 + size - 1;
+
+        x2 = x1;
+    }
+    // Sledeci problem: brod nikad nece biti u malim koordinatama osim ako mu nije pocetak tu???!
+
+    /*
+    if(x1 < (rand()%8))
+    {
+        x2 = x1;
+        y2 = y1 + size - 1;
+    }
+    else
+    {
+        y2 = y1;
+        x2 = x1 + size - 1;
+    }
+    */
+    cout << "Za pocetak da vidimo kako radi:" << endl;
+    cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << endl;
+
+    Field::createShip(x1, y1, x2, y2, f);
+
+}
+
+// Crtanje matrice (nakon svakog gadjanja)
+void Field::printSea(vector <vector <Field> > &f)
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Kad se ubace koordinate 7, 7 ostane neka boja aktivna
+
+    for(int i = 0; i < f.size(); i++)
+    {
+        for(int j = 0; j < f[0].size(); j++)
+        {
+
+            if(f[i][j].isHit())					// Provera da li je dato Polje pogodjeno
+                if(f[i][j].getId() != 0)		// Provera da li je dato Polje zauzeto Brodom
+                {
+                    SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+                    cout << "x ";
+                }
+                else
+                {
+                    SetConsoleTextAttribute(h, FOREGROUND_RED |
+                        FOREGROUND_INTENSITY);
+                    cout << "x ";
+                }
+            else
+            {
+                SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED |
+                    FOREGROUND_INTENSITY);
+                cout << "o ";
+            }
+        }
+        cout << endl;
+    }
+    // Ukoliko se unesu koordinate 7, 7 poslednja promena boje moze biti RED ili GREEN
+    // Resenje: na kraju uvek vracanje na staro ====>
+    SetConsoleTextAttribute(h, FOREGROUND_BLUE |
+                FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+
+}
